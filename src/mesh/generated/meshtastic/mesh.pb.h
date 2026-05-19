89340feb03e262954c1745558583e4b2b49d02f9
daava345-maker <daava345@gmail.com>
2026-05-19 13:16:03 -0600

    Update mesh.pb.h

--- src/mesh/generated/meshtastic/mesh.pb.h
+++ src/mesh/generated/meshtastic/mesh.pb.h
@@ -339,7 +339,7 @@ typedef enum _meshtastic_Constants {
     /* From mesh.options
  note: this payload length is ONLY the bytes that are sent inside of the Data protobuf (excluding protobuf overhead). The 16 byte header is
  outside of this envelope */
-    meshtastic_Constants_DATA_PAYLOAD_LEN = 233
+    meshtastic_Constants_DATA_PAYLOAD_LEN = 400
 } meshtastic_Constants;
 
 /* Error codes for critical errors
